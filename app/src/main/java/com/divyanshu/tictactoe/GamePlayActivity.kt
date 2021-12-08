aaa3b16f7ffd190148bd8acae00151ff00db2393
Divyanshu <divyanshujha02@yahoo.com>
2021-12-08 19:58:31 +0530

    MainActivity Coded

---
+++ app/src/main/java/com/divyanshu/tictactoe/GamePlayActivity.kt
@@ -0,0 +1,11 @@
+package com.divyanshu.tictactoe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GamePlayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_play)
+    }
+}
--- app/src/main/java/com/divyanshu/tictactoe/MainActivity.kt
+++ app/src/main/java/com/divyanshu/tictactoe/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.divyanshu.tictactoe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
+var oneplayer : Boolean = false
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var player1 = findViewById<Button>(R.id.btnsinleplayer)
+        var player2 = findViewById<Button>(R.id.btnmultiplayer)
+
+        player1.setOnClickListener {
+            oneplayer = true
+            startActivity(Intent(this , GamePlayActivity::class.java))
+        }
+
+        player2.setOnClickListener {
+            oneplayer = false
+            startActivity(Intent(this , GamePlayActivity::class.java))
+        }
     }
 }
